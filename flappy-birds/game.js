ae172002b9e6f898fc42c1e62aa762e4fac1c0ed
Muchan Li <limuchan28@gmail.com>
2022-07-27 12:20:16 -0700

    general game layout is ready
    Can be played, but not great yet;
    Missing the sprites for the bird and the coin.

--- flappy-birds/game.js
+++ flappy-birds/game.js
@@ -14,17 +14,55 @@ class mainScene {
         this.score = 0;
         let style = { font: '20px Arial', fill: '#fff' };
         this.scoreText = this.add.text(20, 20, 'score: ' + this.score, style);
+
+        this.arrow = this.input.keyboard.createCursorKeys();
     }
 
     update() {
 
+        // check if coin is hit by the player
+        if (this.physics.overlap(this.player, this.coin)) {
+            // Call the new hit() method
+            this.hit();
+        }
+
+        // horizontal movement
+        if (this.arrow.right.isDown) {
+            this.player.x += 3;
+        } else if (this.arrow.left.isDown) {
+            this.player.x -= 3;
+        } 
+          
+        // vertical movement
+        if (this.arrow.down.isDown) {
+            this.player.y += 3;
+        } else if (this.arrow.up.isDown) {
+            this.player.y -= 3;
+        } 
     }
+
+    hit() {
+        // Change the position x and y of the coin randomly
+        this.coin.x = Phaser.Math.Between(50, 850);
+        this.coin.y = Phaser.Math.Between(50, 600);
+      
+        this.score += 10;
+        this.scoreText.setText('score: ' + this.score);
+
+        this.tweens.add({
+            targets: this.player, // on the player 
+            duration: 200, // for 200ms 
+            scaleX: 1.2, // that scale vertically by 20% 
+            scaleY: 1.2, // and scale horizontally by 20% 
+            yoyo: true, // at the end, go back to original scale 
+          });
+      }
 
 }
 
 new Phaser.Game({
-    width: 1600, // Width of the game in pixels
-    height: 900, // Height of the game in pixels
+    width: 900, // Width of the game in pixels
+    height: 650, // Height of the game in pixels
     backgroundColor: '#D3CEDF', // The background color 
     scene: mainScene, // The name of the scene we created
     physics: { default: 'arcade' }, // The physics engine to use
